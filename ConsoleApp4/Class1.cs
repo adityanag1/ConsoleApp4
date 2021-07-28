08205c5cf80ee8d825698a8b18177b6b4571a327
adityanag1 <adityanag33@gmail.com>
2021-07-28 10:07:02 +0530

    secind

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Class1
+    {
+        public static void main()
+        {
+            Console.WriteLine("hello guys");
+        }
+        
+    }
+}
